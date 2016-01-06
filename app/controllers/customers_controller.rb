2bd927d1d73135fcd665ba2acf1703bdd922ce35
tomoya <ihatov08@gmail.com>
2016-01-07 06:14:55 +0900

    kaminariの追加 fakerの追加 companyモデルの追加 アソシエーション図の作成 プルダウンメニューの追加 before_actionの追加

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,8 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy ]
   def index
-    @customers = Customer.all
+    # @customers = Customer.all
+    @customers = Customer.page(params[:page])
   end
 
   def new
@@ -17,11 +19,11 @@ class CustomersController < ApplicationController
   end
 
   def edit
-    @customer = Customer.find(params[:id])
+    # @customer = Customer.find(params[:id])
   end
 
   def update
-    @customer = Customer.find(params[:id])
+    # @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
       redirect_to @customer
     else
@@ -30,11 +32,11 @@ class CustomersController < ApplicationController
   end
 
   def show
-    @customer = Customer.find(params[:id])
+    # @customer = Customer.find(params[:id])
   end
 
   def destroy
-    @customer = Customer.find(params[:id])
+    # @customer = Customer.find(params[:id])
     @customer.destroy
     redirect_to customers_url
   end
@@ -44,11 +46,13 @@ class CustomersController < ApplicationController
     params.require(:customer).permit(
       :family_name,
       :given_name,
-      :email)
+      :email,
+      :company_id
+      )
   end
 
-
-
-
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,8 +3,10 @@ class Customer < ActiveRecord::Base
     full_name = family_name + given_name + sama
     return full_name
   end
+  belongs_to :company
     validates :family_name, presence: true, length: { maximum: 20 }
   validates :given_name,    presence: true, length: { maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,         presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :company_id, presence: true
 end
