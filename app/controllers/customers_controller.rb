a717903c3774b3aeae3cf17504b674cd882babde
tomoya <ihatov08@gmail.com>
2016-01-07 21:25:23 +0900

    ログインしてから修正、アラート表示

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,7 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy ]
+  # before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy ]
   def index
     # @customers = Customer.all
     # @customers = Customer.page(params[:page])
