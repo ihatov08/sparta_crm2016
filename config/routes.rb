7265ae2243de69fc5af26443a240f5fdce8bcb1d
tomoya <ihatov08@gmail.com>
2016-01-09 22:56:53 +0900

    commentモデルの開発１（コメントフォーム）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :customers
+  # post '/comments' => 'comments#create'
+  resources :comments, only: [:create]
   root 'customers#index'
   # get 'customers/index'
 
